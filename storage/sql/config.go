ff7763560d6396df74abdde143f268f7664b23a4
chub <chub@users.noreply.github.com>
2017-06-30 13:51:19 -0700

    storage: Add MySQL support

--- storage/sql/config.go
+++ storage/sql/config.go
@@ -145,3 +145,80 @@ func (p *Postgres) open(logger logrus.FieldLogger) (*conn, error) {
 	}
 	return c, nil
 }
+
+// MySQL options for creating an SQL db.
+type MySQL struct {
+	Database string
+	User     string
+	Port     int
+	Unix     string
+	Password string
+	Host     string
+}
+
+// Open creates a new storage implementation backed by MySQL.
+func (p *MySQL) Open(logger logrus.FieldLogger) (storage.Storage, error) {
+	conn, err := p.open(logger)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func (p *MySQL) open(logger logrus.FieldLogger) (*conn, error) {
+	v := url.Values{}
+	set := func(key, val string) {
+		if val != "" {
+			v.Set(key, val)
+		}
+	}
+
+	MySQLHost := ""
+	if p.Host == "" && p.Unix == "" {
+		return nil, fmt.Errorf("MySQL needs either the hostname or the unix socket to be set")
+	} else if p.Host != "" {
+		port := strconv.Itoa(p.Port)
+		MySQLHost = "tcp(" + p.Host + ":" + port + ")"
+	} else if p.Unix != "" {
+		MySQLHost = "unix(" + p.Unix + ")"
+	}
+
+	set("multiStatements", "true")
+	set("parseTime", "true")
+	set("loc", "UTC")
+	set("tls", "skip-verify")
+
+	Scheme := "mysql"
+	u := url.URL{
+		Scheme:   Scheme,
+		Host:     MySQLHost,
+		Path:     "/" + p.Database,
+		RawQuery: v.Encode(),
+	}
+
+	if p.User != "" {
+		if p.Password != "" {
+			u.User = url.UserPassword(p.User, p.Password)
+		} else {
+			u.User = url.User(p.User)
+		}
+	}
+	db, err := sql.Open("mysql", u.String()[len(Scheme)+3:])
+	if err != nil {
+		return nil, err
+	}
+
+	errCheck := func(err error) bool {
+		sqlErr, ok := err.(*pq.Error)
+		if !ok {
+			return false
+		}
+		return sqlErr.Code == pgErrUniqueViolation
+	}
+
+	c := &conn{db, flavorMySQL, logger, errCheck}
+	if _, err := c.migrate(); err != nil {
+		return nil, fmt.Errorf("failed to perform migrations: %v", err)
+	}
+	return c, nil
+}
