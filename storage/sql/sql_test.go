ba60e5925d37bf44a6d01bc9d0d01447a8b1f354
chub <chub@users.noreply.github.com>
2017-06-30 13:51:19 -0700

    storage: Add MySQL support

--- storage/sql/sql_test.go
+++ storage/sql/sql_test.go
@@ -2,7 +2,7 @@ package sql
 
 import "testing"
 
-func TestTranslate(t *testing.T) {
+func TestSQLiteTranslate(t *testing.T) {
 	tests := []struct {
 		testCase string
 		flavor   flavor
@@ -44,6 +44,64 @@ func TestTranslate(t *testing.T) {
 			flavorSQLite3,
 			`now(),`,
 			`date('now'),`,
+		},
+	}
+
+	for _, tc := range tests {
+		if got := tc.flavor.translate(tc.query); got != tc.exp {
+			t.Errorf("%s: want=%q, got=%q", tc.testCase, tc.exp, got)
+		}
+	}
+}
+
+func TestMySQLTranslate(t *testing.T) {
+	tests := []struct {
+		testCase string
+		flavor   flavor
+		query    string
+		exp      string
+	}{
+		{
+			"MySQL query bind replacement",
+			flavorMySQL,
+			`select foo from bar where foo.zam = $1;`,
+			`select foo from bar where foo.zam = ?;`,
+		},
+		{
+			"MySQL query bind replacement at newline",
+			flavorMySQL,
+			`select foo from bar where foo.zam = $1`,
+			`select foo from bar where foo.zam = ?`,
+		},
+		{
+			"MySQL create table with boolean column",
+			flavorMySQL,
+			`CREATE TABLE test (something boolean);`,
+			`CREATE TABLE test (something tinyint(1));`,
+		},
+		{
+			"MySQL query true",
+			flavorMySQL,
+			`select foo from bar where foo.zam = true`,
+			`select foo from bar where foo.zam = 1`,
+		},
+		{
+			"MySQL query false",
+			flavorMySQL,
+			`select foo from bar where foo.zam = false`,
+			`select foo from bar where foo.zam = 0`,
+		},
+		{
+			"MySQL bytea",
+			flavorMySQL,
+			`"connector_data" bytea not null,`,
+			`"connector_data" blob not null,`,
+		},
+		{
+			"MySQL now",
+			flavorMySQL,
+			`now(),`,
+			`now(),`,
 		},
 	}
 
